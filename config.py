01555664228d1337d7e0eccf0b98b31614677735
Sidiko <128436866+pkmnSidiko@users.noreply.github.com>
2026-09-19 17:24:29 -0400

    Add default configuration settings

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+DEFAULT_CONFIG = {
+    "timezone": "UTC",
+    "xp_enabled": True,
+    "announcement_channel_id": None,
+    "writing_channel_id": None,
+}
 
+
+def get_default_config():
+    return DEFAULT_CONFIG.copy()
